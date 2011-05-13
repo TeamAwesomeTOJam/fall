01a772c290b889785f5aaebaab521b847ecd1f30
Jonathan Doda <jonathan@jdoda.ca>
2011-05-13 13:09:31 -0400

    First commit.

---
+++ src/stickman.py
@@ -0,0 +1,17 @@
+'''
+Created on May 13, 2011
+
+@author: jonathan
+'''
+
+class StickMan(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
